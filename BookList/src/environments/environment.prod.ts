60b8dd5e08581802853bdc4be5f0fe7a2bf0dfe7
Maycon Ribeiro <maycon_ribeiro@hotmail.com>
2019-05-02 20:03:50 -0300

    remove BookList

--- BookList/src/environments/environment.prod.ts
+++
@@ -1,8 +0,0 @@
-export const environment = {
-  production: true,
-  environments: {
-    "source": "environments/environment.ts",
-    "dev": "environments/environment.ts",
-    "prod": "environments/environment.prod.ts"
-  }
-};
